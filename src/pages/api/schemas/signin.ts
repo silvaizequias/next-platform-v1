5709ba3a1e252bfec45d005a3facdc122ba2bfd6
Izequias Silva <master@sistemadedicado.com>
2023-02-27 14:52:11 -0300

    rebase

--- src/pages/api/schemas/signin.ts
+++
@@ -1,8 +0,0 @@
-import { object, string, TypeOf } from 'yup'
-
-export const signInSchema = object({
-  email: string().required().email().max(200),
-  password: string().required().min(6).max(25)
-})
-
-export type SignIn = TypeOf<typeof signInSchema>
--- src/views/pages/landing/LandingPage.tsx
+++
@@ -1,9 +0,0 @@
-import Header from './Header'
-
-export default function LandingPage() {
-  return (
-    <>
-      <Header />
-    </>
-  )
-}
