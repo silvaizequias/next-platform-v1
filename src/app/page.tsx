d82dc0de054a77dbf93cae70c98df7e869b5815d
Izequias Silva <silvaizequias@icloud.com>
2023-08-18 13:11:56 -0300

    building with nextjs 13.4.18

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+  title: 'Dedicado Digital',
+  description: 'Sistema Dedicado Personalizado',
+}
+
+export default async function MainPage() {
+  return ''
+}
