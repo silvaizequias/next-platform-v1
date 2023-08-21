7a5911d26c3e4321eff141010724185deecace28
Izequias Silva <silvaizequias@icloud.com>
2023-08-21 09:24:01 -0300

    remove styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,15 +6,6 @@ const config: Config = {
     './src/views/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
   plugins: [],
 }
 export default config
