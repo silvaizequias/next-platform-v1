ee9a048d3b6d56fb76b7d47993fade326229ab5c
Izequias Silva <silvaizequias@icloud.com>
2023-08-25 10:14:46 -0300

    openai stream implemented

---
+++ src/app/api/openai-stream/route.ts
@@ -0,0 +1,30 @@
+import { openai } from '@/libraries/openai'
+import { OpenAiStreamSchema, OpenAiStreamSchemaType } from '@/schemas/openai'
+
+export const POST = async (request: Request) => {
+  try {
+    return await request.json().then(async (inputs: OpenAiStreamSchemaType) => {
+      if (OpenAiStreamSchema.validateSync(inputs)) {
+        const { content, stream } = inputs
+
+        //TODO: Sistema de conversas ainda precisa ser melhorado
+        const data: any = (await openai()).chat.completions.create({
+          model: 'gpt-4',
+          messages: [{ role: 'user', content: content }],
+          stream: stream,
+        })
+        
+        for await (const part of [data]) {
+            console.log(process.stdout.write(part.choices[0]?.delta?.content || ''))
+          return new Response(
+            JSON.stringify(
+              process.stdout.write(part.choices[0]?.delta?.content || ''),
+            ),
+          )
+        }
+      }
+    })
+  } catch (error: any) {
+    return new Response(error?.message || error, { status: 400 })
+  }
+}
