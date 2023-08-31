d0de100ada6ee1f9d3555d2c917adfbbc95af616
Izequias Silva <silvaizequias@icloud.com>
2023-08-31 18:20:53 -0300

    alert removed

--- src/components/AlertMessage/types/index.ts
+++
@@ -1,8 +0,0 @@
-import { AlertColor } from '@mui/material'
-
-export interface AlertMessageProps {
-  message: string
-  severity?: AlertColor | undefined
-  positionY?: "top" | "bottom"
-  positionX?: "center" | "right" | "left"
-}
