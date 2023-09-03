08bf43ed42f378d6ce2981a1ec50e2198bf05f90
Izequias Silva <silvaizequias@icloud.com>
2023-09-03 17:48:21 -0300

    update

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,6 +1,11 @@
+import { authOptions } from '@/libraries/next-auth'
+import { getServerSession } from 'next-auth'
+
 export const GET = async (request: Request) => {
+  const session = await getServerSession(authOptions)
+
   try {
-    return new Response(request.method)
+    return new Response(JSON.stringify(session))
   } catch (error: any) {
     return new Response(error?.message || error, {
       status: 400,
