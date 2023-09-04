f4a8d4774926069c5afe2ab57eb750a872eb3b59
Izequias Silva <silvaizequias@icloud.com>
2023-09-04 16:18:02 -0300

    vercel analytics implemented

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,18 +7,22 @@ import { LayoutProps } from '@/types'
 import { getServerSession } from 'next-auth'
 import { Suspense } from 'react'
 import Providers from './providers'
+import { Analytics } from '@vercel/analytics/react'
 
 export default async function AppLayout(props: LayoutProps) {
   const { children } = props
   const session = await getServerSession(authOptions)
 
+  const onDevelopment = process.env.NODE_ENV === 'development'
+
   return (
     <Providers>
       <html lang='en' suppressHydrationWarning>
-        <CrispChatProvider />
+        {!onDevelopment && <CrispChatProvider />}
         <body>
           {session && <TopBar session={session} />}
           <Suspense fallback={<Spinner />}>{children}</Suspense>
+          {!onDevelopment && <Analytics />}
           <ToastProvider />
         </body>
       </html>
