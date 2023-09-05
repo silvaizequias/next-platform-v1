e7a2aae9f512a85338b0ca55ad4b8c01fe4ff8de
Izequias Silva <silvaizequias@icloud.com>
2023-09-05 18:03:48 -0300

    stripe webhook

--- src/app/api/stripe/webhook/route.ts
+++ src/app/api/stripe/webhook/route.ts
@@ -1,3 +1,6 @@
+import { stripe } from '@/libraries/stripe'
+import { NextApiRequest } from 'next'
+
 export const GET = async (request: Request) => {
   try {
     return new Response(JSON.stringify(request.method))
@@ -6,10 +9,33 @@ export const GET = async (request: Request) => {
   }
 }
 
-export const POST = async (request: Request) => {
+export const POST = async (request: NextApiRequest) => {
+  const sig = request.headers['stripe-signature']!
+  const buf = new Buffer(request.body)
+  let event
+
+  const STRIPE_WEBHOOK_SECRET = process.env.STRIPE_WEBHOOK_SECRET!
   try {
-    return new Response(JSON.stringify(request.method))
+    event = stripe.webhooks.constructEvent(buf, sig, STRIPE_WEBHOOK_SECRET)
   } catch (error: any) {
     return new Response(error?.message || error, { status: 400 })
   }
+
+  // Handle the event
+  switch (event.type) {
+    case 'payment_intent.succeeded':
+      const paymentIntent = event.data.object
+      console.log('PaymentIntent was successful!')
+      break
+    case 'payment_method.attached':
+      const paymentMethod = event.data.object
+      console.log('PaymentMethod was attached to a Customer!')
+      break
+    // ... handle other event types
+    default:
+      console.log(`Unhandled event type ${event.type}`)
+  }
+
+  // Return a response to acknowledge receipt of the event
+  return new Response(JSON.stringify({ received: true }))
 }
