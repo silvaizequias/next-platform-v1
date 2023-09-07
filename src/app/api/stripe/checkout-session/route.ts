eae7af5dc64c54818d989f1c741f6195f2548192
Izequias Silva <silvaizequias@icloud.com>
2023-09-07 17:46:41 -0300

    checkout session implemented

--- src/app/api/stripe/checkout-session/route.ts
+++ src/app/api/stripe/checkout-session/route.ts
@@ -1,3 +1,4 @@
+import { prisma } from '@/libraries/prisma'
 import { stripe } from '@/libraries/stripe'
 import {
   StripeCheckoutSchema,
@@ -5,23 +6,52 @@ import {
 } from '@/schemas/stripe'
 
 export const POST = async (request: Request) => {
+  //TODO: criar função de caminho absoluto
   const NEXTAUTH_URL = process.env.NEXTAUTH_URL!
+
   try {
+    await prisma?.$connect()
     return await request
       .json()
       .then(async (inputs: StripeCheckoutSchemaType) => {
         if (await StripeCheckoutSchema.parseAsync(inputs)) {
-          const { amount, service, description, userId, userEmail } = inputs
-          const session = await stripe.checkout.sessions.create({
+          const {
+            serviceId,
+            serviceName,
+            serviceDescription,
+            serviceAmount,
+            userId,
+            userEmail,
+          } = inputs
+
+          const subscription = await prisma?.subscription.findFirst({
+            where: { userId },
+          })
+          if (subscription && subscription?.stripeCustomerId) {
+            const stripeSession = await stripe.billingPortal.sessions.create({
+              customer: subscription?.stripeCustomerId,
+              return_url: NEXTAUTH_URL,
+            })
+
+            return new Response(JSON.stringify({ url: stripeSession?.url }))
+          }
+
+          const stripeSession = await stripe.checkout.sessions.create({
+            mode: 'subscription',
+            payment_method_types: ['card'],
+            billing_address_collection: 'auto',
+            customer_email: userEmail,
+            success_url: `${NEXTAUTH_URL}/?success=true`, //TODOS: criar rota de configurações do usuário
+            cancel_url: `${NEXTAUTH_URL}/?canceled=true`,
             line_items: [
               {
                 price_data: {
                   currency: 'brl',
                   product_data: {
-                    name: service,
-                    description: description,
+                    name: serviceName,
+                    description: serviceDescription,
                   },
-                  unit_amount: amount * 100,
+                  unit_amount: serviceAmount * 100, //TODO: criar função de cálculo para preço de acordo com ointervalo
                   recurring: {
                     interval: 'month',
                   },
@@ -31,19 +61,16 @@ export const POST = async (request: Request) => {
             ],
             metadata: {
               userId,
+              serviceId,
             },
-            mode: 'subscription',
-            payment_method_types: ['card'],
-            billing_address_collection: 'auto',
-            customer_email: userEmail,
-            success_url: `${NEXTAUTH_URL}/?success=true`,
-            cancel_url: `${NEXTAUTH_URL}/?canceled=true`,
           })
-          return new Response(JSON.stringify(session))
+          return new Response(JSON.stringify(stripeSession))
         }
       })
   } catch (error: any) {
     console.error('[STRIPE_CHECKOUT_ERROR]: ', error)
     return new Response(error?.message || error, { status: 400 })
+  } finally {
+    await prisma?.$disconnect()
   }
 }
