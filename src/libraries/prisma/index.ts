d2d279a62bdcf4f5820cf7f064ceeab014a4739e
Izequias Silva <silvaizequias@icloud.com>
2023-09-12 00:15:56 -0300

    database implemented

--- src/libraries/prisma/index.ts
+++ src/libraries/prisma/index.ts
@@ -1,10 +1,15 @@
 import { PrismaClient } from '@prisma/client'
+import { PrismaClient as PrismaClientDriving } from '../../../node_modules/@schema-generations/client/driving'
 
 declare global {
   var prisma: PrismaClient | undefined
+  var prismaDriving: PrismaClientDriving | undefined
 }
 
 export const prisma = global.prisma || new PrismaClient()
+export const prismaDriving = global.prismaDriving || new PrismaClientDriving()
 
-if (process.env.NODE_ENV === 'development') global.prisma = prisma
-
+if (process.env.NODE_ENV === 'development') {
+  global.prisma = prisma
+  global.prismaDriving = prismaDriving
+}
