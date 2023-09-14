cdbb8d74654fddd77a157ab1a1adbccef286e579
Izequias Silva <silvaizequias@icloud.com>
2023-09-14 12:38:20 -0300

    (feature): seo

---
+++ src/app/robots.ts
@@ -0,0 +1,14 @@
+import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  const NEXTAUTH_URL = process.env.NEXTAUTH_URL!
+
+  return {
+    rules: {
+      userAgent: '*',
+      allow: ['/', '/blog', '/blog/*'],
+      disallow: ['/api', 'control'],
+    },
+    sitemap: NEXTAUTH_URL,
+  }
+}
