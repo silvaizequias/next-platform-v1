770f0af07f48a3e37fd7f9e82ae1fd7d8de1c27d
Izequias Silva <silvaizequias@icloud.com>
2023-09-19 15:34:37 -0300

    (feature): page view implementations

---
+++ src/app/(solutions)/gestao-de-servicos/page.tsx
@@ -0,0 +1,23 @@
+import { authOptions } from '@/libraries/next-auth'
+import ServiceManagementPrivateView from '@/views/solutions/service-management/ServiceManagementPrivateView'
+import ServiceManagementPublicView from '@/views/solutions/service-management/ServiceManagementPublicView'
+import { Metadata } from 'next'
+import { getServerSession } from 'next-auth'
+
+export const metadata: Metadata = {
+  title: 'Gestão de Serviços',
+}
+
+export default async function FieldServicePage() {
+  const session = await getServerSession(authOptions)
+
+  return (
+    <main>
+      {session ? (
+        <ServiceManagementPrivateView session={session!} metadata={metadata!} />
+      ) : (
+        <ServiceManagementPublicView />
+      )}
+    </main>
+  )
+}
---
+++ src/views/solutions/service-management/ServiceManagementPrivateView.tsx
@@ -0,0 +1,7 @@
+import { PageViewProps } from '@/types'
+
+export default function ServiceManagementPrivateView(props: PageViewProps) {
+  const { session, metadata } = props
+
+  return ''
+}
