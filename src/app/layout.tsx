b91e2f4e2460bca9ad29748c769f27707fe77caf
Izequias Silva <silvaizequias@icloud.com>
2023-09-20 12:38:27 -0300

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Dedicado Digital',
+  description: 'Soluções personalizadas de sistemas de alta performance que aumentam a produtividade de pessoas e organizações',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
