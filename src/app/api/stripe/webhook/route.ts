75a4141e6278155741843909dd736a9886583e92
Izequias Silva <silvaizequias@icloud.com>
2023-09-25 14:47:03 -0300

    stripe webhook implemented

---
+++ src/app/api/stripe/webhook/route.ts
@@ -0,0 +1,79 @@
+import { stripe } from '@/libraries/stripe'
+import { prisma } from '@/libraries/prisma'
+import { headers } from 'next/headers'
+import Stripe from 'stripe'
+
+export const GET = async (request: Request) => {
+  try {
+    return new Response(JSON.stringify(request.method))
+  } catch (error: any) {
+    return new Response(error?.message || error, { status: 400 })
+  }
+}
+
+export const POST = async (request: Request) => {
+  const body = await request.text()
+  const signature = headers().get('Stripe-Signature') as string
+  const STRIPE_WEBHOOK_SECRET = process.env.STRIPE_WEBHOOK_SECRET!
+
+  let event: Stripe.Event
+
+  try {
+    event = stripe.webhooks.constructEvent(
+      body,
+      signature,
+      STRIPE_WEBHOOK_SECRET,
+    )
+  } catch (error: any) {
+    return new Response(error?.message || error, { status: 400 })
+  }
+
+  const session = event.data.object as Stripe.Checkout.Session
+
+  if (event.type === 'checkout.session.completed') {
+    if (!session?.metadata?.userId && !session?.metadata?.solutionId) {
+      return new Response('o usuario e a solução não foram definidos', {
+        status: 400,
+      })
+    }
+
+    const subscription = await stripe.subscriptions.retrieve(
+      session.subscription as string,
+    )
+
+    await prisma.subscription.create({
+      data: {
+        userId: session?.metadata?.userId,
+        solutionId: session?.metadata?.solutionId,
+        stripeCustomerId: subscription.customer as string,
+        stripeSubscriptionId: subscription.id,
+        stripePriceId: subscription.items.data[0].price.id,
+        currentPeriodStart: new Date(subscription.current_period_start * 1000),
+        currentPeriodEnd: new Date(subscription.current_period_end * 1000),
+        discount: Number(session?.metadata?.discount),
+        tax: Number(session?.metadata?.tax),
+        amount: Number(session?.metadata?.amount),
+      },
+    })
+  }
+
+  if (event.type === 'invoice.payment_succeeded') {
+    const subscription = await stripe.subscriptions.retrieve(
+      session.subscription as string,
+    )
+
+    await prisma.subscription.update({
+      where: { stripeCustomerId: subscription.id },
+      data: {
+        stripePriceId: subscription.items.data[0].price.id,
+        currentPeriodStart: new Date(subscription.current_period_start * 1000),
+        currentPeriodEnd: new Date(subscription.current_period_end * 1000),
+        discount: Number(session?.metadata?.discount),
+        tax: Number(session?.metadata?.tax),
+        amount: Number(session?.metadata?.amount),
+      },
+    })
+  }
+
+  return new Response(null, { status: 201 })
+}
