50e1d673885a3bd5e3af520e44ca90c5e0cab8d2
Izequias Silva <silvaizequias@icloud.com>
2023-09-29 18:33:12 -0300

    providers updated

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,10 +1,13 @@
 'use client'
 
+import ToastProvider from '@/components/toast-provider'
 import { LayoutProps } from '@/layouts/types'
 import themeOptions from '@/theme'
 import NextAppDirEmotionCacheProvider from '@/theme/EmotionCache'
 import { CssBaseline } from '@mui/material'
 import { ThemeProvider } from '@mui/system'
+import { LocalizationProvider } from '@mui/x-date-pickers'
+import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment'
 import { SessionProvider } from 'next-auth/react'
 
 export default function Providers(props: LayoutProps) {
@@ -15,7 +18,10 @@ export default function Providers(props: LayoutProps) {
       <NextAppDirEmotionCacheProvider options={{ key: 'mui' }}>
         <ThemeProvider theme={themeOptions}>
           <CssBaseline />
-          {children}
+          <LocalizationProvider dateAdapter={AdapterMoment}>
+            {children}
+            <ToastProvider />
+          </LocalizationProvider>
         </ThemeProvider>
       </NextAppDirEmotionCacheProvider>
     </SessionProvider>
