434d30d8a25d1393826124c096d5d5135d5d52a6
Izequias Silva <silvaizequias@icloud.com>
2023-10-02 09:56:33 -0300

    muit lab

--- src/views/auth/AuthTabsView.tsx
+++ src/views/auth/AuthTabsView.tsx
@@ -1,5 +1,10 @@
+import { TabContext, TabList, TabPanel } from '@mui/lab'
+import { Box, Tab } from '@mui/material'
 import { SyntheticEvent, useState } from 'react'
 import AuthSignInForm from './forms/AuthSignInForm'
+import AuthSignUpForm from './forms/AuthSignUpForm'
+import AuthResetPasswordForm from './forms/AuthResetPasswordForm'
+import { MdBadge, MdOutlineLogin, MdOutlinePassword } from 'react-icons/md'
 import { signIn } from 'next-auth/react'
 
 export default function AuthTabsView() {
@@ -14,6 +19,40 @@ export default function AuthTabsView() {
   }
 
   return (
-    <div><AuthSignInForm /></div>
+    <Box sx={{ width: '100%', typography: 'body1' }}>
+      <TabContext value={value}>
+        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+          <TabList onChange={handleChange} centered>
+            <Tab
+              icon={<MdOutlineLogin />}
+              iconPosition='start'
+              label='Acessar'
+              value='sign-in'
+            />
+            <Tab
+              icon={<MdBadge />}
+              iconPosition='start'
+              label='Registrar'
+              value='sign-up'
+            />
+            <Tab
+              icon={<MdOutlinePassword />}
+              iconPosition='start'
+              label='Redefinir'
+              value='reset-password'
+            />
+          </TabList>
+        </Box>
+        <TabPanel value='sign-in'>
+          <AuthSignInForm />
+        </TabPanel>
+        <TabPanel value='sign-up'>
+          <AuthSignUpForm />
+        </TabPanel>
+        <TabPanel value='reset-password'>
+          <AuthResetPasswordForm />
+        </TabPanel>
+      </TabContext>
+    </Box>
   )
 }
