9a15d9954db284c735a9ad5f0ede248041f9606e
Izequias Silva <silvaizequias@icloud.com>
2023-10-02 06:45:34 -0300

    sign up updated

--- src/types/auth/schema.ts
+++ src/types/auth/schema.ts
@@ -11,6 +11,7 @@ export const AuthSignUpSchema = z.object({
   name: z.string(),
   phone: z.string().length(11),
   email: z.string().email(),
+  password: z.string().min(8).max(25).optional()
 })
 
 export type AuthSignUpSchemaType = z.infer<typeof AuthSignUpSchema>
