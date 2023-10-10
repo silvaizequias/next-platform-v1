0a221ea9b8292c6e6ad8a003463ac93e23d0068d
Izequias Silva <silvaizequias@icloud.com>
2023-10-10 10:32:26 -0300

    set cors

--- src/helpers/api.ts
+++ src/helpers/api.ts
@@ -7,6 +7,11 @@ export const api = axios.create({
   baseURL: NEXTAUTH_URL,
   //withCredentials: true,
   //headers: {
+  //  'Access-Control-Allow-Credentials': 'true',
+  //  'Access-Control-Allow-Origin': '*',
+  //  'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+  //  'Access-Control-Allow-Headers':
+  //    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, //Content-MD5, //Content-Type, Date, X-Api-Version',
   //  Authorization: `${NEXT_PUBLIC_DEDICATED_API_KEY}`,
   //},
 })
