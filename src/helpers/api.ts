8865d1f89f649a3316f5503dae42a7179da5d205
Izequias Silva <silvaizequias@icloud.com>
2023-10-10 09:16:53 -0300

    cors implemented

--- src/helpers/api.ts
+++ src/helpers/api.ts
@@ -5,6 +5,7 @@ const NEXT_PUBLIC_DEDICATED_API_KEY = process.env.NEXT_PUBLIC_DEDICATED_API_KEY!
 
 export const api = axios.create({
   baseURL: NEXTAUTH_URL,
+  //withCredentials: true,
   //headers: {
   //  Authorization: `${NEXT_PUBLIC_DEDICATED_API_KEY}`,
   //},
