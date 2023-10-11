48592e7acad88e691086792a22276f7195bb42aa
Izequias Silva <silvaizequias@icloud.com>
2023-10-11 09:40:36 -0300

    header implemented

--- src/views/solutions/service-management/ServiceManagementPublicView.tsx
+++ src/views/solutions/service-management/ServiceManagementPublicView.tsx
@@ -1,11 +1,26 @@
-import { Container, Grid, Typography } from '@mui/material'
+import { Box, Container, Divider, Grid, Typography } from '@mui/material'
+import { grey } from '@mui/material/colors'
 
 export default function ServiceManagementPublicView() {
   return (
     <Container maxWidth='xl'>
       <Grid container paddingTop={10} columnSpacing={2} rowGap={2}>
         <Grid item xs={12} sm={12}>
-          <Typography variant='body1'>Service Manager Public View</Typography>
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              textAlign: 'center',
+              mx: 'auto',
+            }}
+          >
+            <Typography variant='h1'>Gestão de Serviços</Typography>
+            <Divider sx={{ m: 4, px: 10, color: grey[50] }} />
+            <Typography variant='button' textTransform={'uppercase'}>
+              Solução inteligente para acompanhamento de demandas em tempo real,
+              que aumentam a produtividade de pessoas e organizações
+            </Typography>
+          </Box>
         </Grid>
       </Grid>
     </Container>
