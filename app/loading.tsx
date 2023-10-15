421f58a0be6b39581a14230be074f92c695e5c07
Izequias Silva <silvaizequias@icloud.com>
2023-10-15 15:24:00 -0300

    update

--- app/loading.tsx
+++ app/loading.tsx
@@ -3,7 +3,7 @@ export default function Loading() {
     <div className='h-screen bg-sky-200 dark:bg-sky-800'>
       <div className='flex min-h-full justify-center items-center'>
         <div className='flex flex-col gap-2'>
-          <p className='text-6xl text-center uppercase text-zinc-800 dark:text-zinc-50'>
+          <p className='text-2xl text-center uppercase text-zinc-600 dark:text-zinc-50'>
             Carregando...
           </p>
         </div>
