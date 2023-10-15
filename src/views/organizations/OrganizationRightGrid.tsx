4971c0b0415ffb04dabc5a48043d238167e67871
Izequias Silva <silvaizequias@icloud.com>
2023-10-15 12:20:19 -0300

    add member implemented

--- src/views/organizations/OrganizationRightGrid.tsx
+++ src/views/organizations/OrganizationRightGrid.tsx
@@ -1,17 +1,54 @@
-import { Box, Card, Typography } from '@mui/material'
+import { Box, Card, IconButton, Tooltip, Typography } from '@mui/material'
 import { OrganizationProps } from './types'
 import MemberDataGrid from './MemberDataGrid'
+import { MdPersonAddAlt } from 'react-icons/md'
+import { grey } from '@mui/material/colors'
+import { useState } from 'react'
+import AddMemberForm from './forms/AddMemberForm'
+import ShowInDialog from '@/components/show-in-dialog'
 
 export default function OrganizationRightGrid(props: OrganizationProps) {
   const { organization } = props
+  const [openDialog, setOpenDialog] = useState<boolean>(false)
+
+  const handleDialog = () => {
+    setOpenDialog(!openDialog)
+  }
 
   return (
     <Box>
-      <Typography
-        variant='h4'
-        textAlign={'center'}
-        marginBottom={4}
-      >{`Membros da Organização ${organization?.name}`}</Typography>
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignContent: 'center',
+          mb: 4,
+        }}
+        onClick={handleDialog}
+      >
+        <Tooltip title='Adicionar Membro'>
+          <IconButton
+            sx={{
+              p: 1,
+              m: 0,
+              color: grey[50],
+            }}
+          >
+            <MdPersonAddAlt />
+          </IconButton>
+        </Tooltip>
+        <Typography
+          variant='h4'
+          sx={{ textAlign: 'center', md: { textAlign: 'right' } }}
+        >{`Membros da Organização ${organization?.name}`}</Typography>
+      </Box>
+      <ShowInDialog
+        open={openDialog}
+        onClose={handleDialog}
+        title={`Adicionar Membro a ${organization?.name!}`}
+      >
+        <AddMemberForm organization={organization!} />
+      </ShowInDialog>
       <Card elevation={4}>
         <MemberDataGrid users={organization?.users!} />
       </Card>
