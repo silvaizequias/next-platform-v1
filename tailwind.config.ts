1ac7f08afc32e7cf0903db6be4b382b8db99569f
Izequias Silva <silvaizequias@icloud.com>
2023-10-17 10:08:53 -0300

    reestructured

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,18 @@
 //import type { Config } from 'tailwindcss'
 import withMT from '@material-tailwind/react/utils/withMT'
 
-module.exports = withMT ({
+module.exports = withMT({
   darkMode: 'class',
   content: [
-    './views/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['var(--font-roboto)']
-      }
+        sans: ['var(--font-roboto)'],
+      },
     },
   },
   plugins: [],
