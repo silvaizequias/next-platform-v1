6cdf96d48d3dbc130fc11e884dcf60431678487c
Izequias Silva <silvaizequias@icloud.com>
2023-10-18 14:41:36 -0300

    refactoring

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,11 +6,11 @@ export default function NotFound() {
       <div className="h-screen flex justify-center items-center">
         <div className="m-8">
           <div className="flex flex-col text-center">
-            <h4 className="uppercase text-xl font-light">Ops!</h4>
-            <p className="uppercase text-xs text-horizon-800">
+            <h4 className="uppercase text-6xl font-light">Ops!</h4>
+            <p className="uppercase text-sm text-horizon-800">
               Esse conteudo nao existe ou foi movido daqui...
             </p>
-            <Link className="text-sm" href="/">
+            <Link className="uppercase text-xs text-horizon-600" href="/">
               Retornar ao inicio
             </Link>
           </div>
