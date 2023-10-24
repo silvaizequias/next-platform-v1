f49605720223867df9f2fac5418a032e4048d6a2
Izequias Silva <silvaizequias@icloud.com>
2023-10-24 20:14:59 -0300

    next-auth secret implemented

--- src/libraries/next-auth/index.ts
+++ src/libraries/next-auth/index.ts
@@ -1,5 +1,8 @@
 import { NextAuthOptions } from 'next-auth'
 
+const NEXTAUTH_SECRET = process.env.NEXTAUTH_SECRET!
+
 export const authOptions: NextAuthOptions = {
+  secret: NEXTAUTH_SECRET,
   providers: [],
 }
