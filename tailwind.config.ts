8a9e13001fbf8a1e2f9610fe30209826a663efd2
Izequias Silva <silvaizequias@icloud.com>
2023-10-29 17:43:10 -0300

    updated

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,7 @@ import { nextui } from '@nextui-org/react'
 const config = {
   darkMode: 'class',
   content: [
+    './src/views/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
     './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
