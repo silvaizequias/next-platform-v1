2102cad32aa90db664e7cc793293aae9797cf5ad
Izequias Silva <silvaizequias@icloud.com>
2023-10-29 12:48:48 -0300

    analytics implemented

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import { ReactNode } from 'react'
 import Providers from './providers'
+import { Analytics } from '@vercel/analytics/react'
 
 const inter = Inter({
   subsets: ['latin'],
@@ -25,10 +26,15 @@ export const metadata: Metadata = {
 }
 
 export default function RootLayout({ children }: { children: ReactNode }) {
+  const isDevelopment = process.env.NODE_ENV === 'development'
+
   return (
     <html lang="en" className={`s ${inter.variable}`}>
       <body className="min-h-screen bg-horizon-50 text-horizon-800 dark:bg-lunar-900 dark:text-lunar-100">
-        <Providers>{children}</Providers>
+        <Providers>
+          {children}
+          {!isDevelopment && <Analytics />}
+        </Providers>
       </body>
     </html>
   )
