a7c962da26799e3f9a9f6b809566f927a175d8ce
Izequias Silva <silvaizequias@icloud.com>
2023-10-30 17:12:10 -0300

    nextui removed

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,15 +2,14 @@
 
 import CrispChat from '@/components/crisp-chat'
 import ToastProvider from '@/components/hot-toast'
-import { NextUIProvider } from '@nextui-org/react'
 import { ReactNode } from 'react'
 
 export default function Providers({ children }: { children: ReactNode }) {
   return (
-    <NextUIProvider>
+    <main>
       {children}
       <ToastProvider />
       <CrispChat />
-    </NextUIProvider>
+    </main>
   )
 }
