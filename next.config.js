f38fbf321261da865313b12cd74bbe09fd34b84f
Izequias Silva <silvaizequias@icloud.com>
2023-10-30 18:41:38 -0300

    set config updated

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: true }
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  modularizeImports: {
+    '@mui/icons-material': {
+      transform: '@mui/icons-material/{{member}}',
+    },
+  },
+}
 
 module.exports = nextConfig
