550f929765e61993b04d5e41722cb24c3e986d13
Izequias Silva <silvaizequias@icloud.com>
2023-10-30 19:00:55 -0300

    globals css removed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,7 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
 import { ReactNode } from 'react'
 import Providers from './providers'
 import { Analytics } from '@vercel/analytics/react'
-
-const inter = Inter({
-  subsets: ['latin'],
-  weight: ['100', '200', '400', '600'],
-  variable: '--font-inter',
-})
-
 const NEXTAUTH_URL = process.env.NEXTAUTH_URL!
 
 export const metadata: Metadata = {
@@ -29,7 +20,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   const isDevelopment = process.env.NODE_ENV === 'development'
 
   return (
-    <html lang="en" className={`s ${inter.variable}`}>
+    <html lang="en">
       <body>
         <Providers>
           {children}
