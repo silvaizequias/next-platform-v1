2eff6acf11597bfc72c383182205254de6414604
Izequias Silva <silvaizequias@icloud.com>
2023-10-30 17:29:34 -0300

    localization provider implemented

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,14 +2,18 @@
 
 import CrispChat from '@/components/crisp-chat'
 import ToastProvider from '@/components/hot-toast'
+import { LocalizationProvider } from '@mui/x-date-pickers'
+import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment'
 import { ReactNode } from 'react'
 
 export default function Providers({ children }: { children: ReactNode }) {
   return (
     <main>
-      {children}
-      <ToastProvider />
-      <CrispChat />
+      <LocalizationProvider dateAdapter={AdapterMoment}>
+        {children}
+        <ToastProvider />
+        <CrispChat />
+      </LocalizationProvider>
     </main>
   )
 }
