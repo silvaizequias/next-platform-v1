58c0526c73c3075ce6ba31894e69eb84b734a992
Izequias Silva <silvaizequias@icloud.com>
2023-11-02 13:39:06 -0300

    bg color updated

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -3,6 +3,7 @@ import { ReactNode } from 'react'
 import Topbar from './components/topbar'
 import { getServerSession } from 'next-auth'
 import { authOptions } from '@/libraries/next-auth'
+import { grey } from '@mui/material/colors'
 
 export default async function AppLayout({ children }: { children: ReactNode }) {
   const session = await getServerSession(authOptions)
@@ -10,7 +11,12 @@ export default async function AppLayout({ children }: { children: ReactNode }) {
   return (
     <Box>
       <Topbar session={session!} />
-      <Box component={'main'} sx={{display: 'flex'}}>{children}</Box>
+      <Box
+        component={'main'}
+        sx={{ display: 'flex', bgcolor: grey[200], minHeight: '100vh' }}
+      >
+        {children}
+      </Box>
     </Box>
   )
 }
