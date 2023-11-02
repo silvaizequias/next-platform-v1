c7cd391f279cf0c2c15a393417df434c31c66ac1
Izequias Silva <silvaizequias@icloud.com>
2023-11-02 13:09:48 -0300

    update

--- src/views/service-management/index.tsx
+++ src/views/service-management/index.tsx
@@ -1,14 +1,11 @@
-import HeadLook from '@/components/head-look'
 import { Grid } from '@mui/material'
+import SessionHero from './sessions/SessionHero'
 
 export default function ServiceManagementView() {
   return (
     <Grid container>
       <Grid item xs={12}>
-        <HeadLook
-          title="Gestão de Serviços"
-          subtitle="Acompanhamento das demandas de serviço em tempo real"
-        />
+        <SessionHero />
       </Grid>
     </Grid>
   )
---
+++ src/views/service-management/sessions/SessionHero.tsx
@@ -0,0 +1,10 @@
+import HeadLook from '@/components/head-look'
+
+export default function SessionHero() {
+  return (
+    <HeadLook
+      title="Gestão de Serviços"
+      subtitle="Acompanhamento das demandas de serviço em tempo real"
+    />
+  )
+}
