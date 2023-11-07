9bb683bb965ee9fceb0aac785f2ad0448f8d0806
Izequias Silva <silvaizequias@icloud.com>
2023-11-07 12:19:22 -0300

    update

--- src/types/auth/schema.ts
+++ src/types/auth/schema.ts
@@ -2,6 +2,6 @@ import * as z from 'zod'
 
 export const AuthSignIn = z.object({
   email: z.string().email(),
-  phone: z.string().length(11),
+  password: z.string().min(8).max(25),
 })
 export type AuthSignInType = z.infer<typeof AuthSignIn>
