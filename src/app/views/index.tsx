5e56f9f19a288d0b680dd685f9305c0e8dcfed09
Izequias Silva <silvaizequias@icloud.com>
2023-11-13 15:32:16 -0300

    update

--- src/app/views/index.tsx
+++ src/app/views/index.tsx
@@ -3,21 +3,21 @@ import PageSection from '@/components/page-section'
 
 export default function LandingView() {
   return (
-    <div className="flex flex-col justify-center sm:w-screen">
-      <div className="flex flex-grow items-center justify-center h-screen">
+    <div className="flex flex-wrap flex-auto flex-col w-screen">
+      <div className="flex items-center justify-center h-screen mx-auto">
         <PageSection
           title="Suporte especializado"
           subtitle="experimente um atendimento totalmente personalizado"
           description="O melhor apoio técnico para sua operação está aqui"
         />
       </div>
-      <div className="flex flex-grow items-center justify-center h-screen">
+      <div className="flex items-center justify-center h-screen">
         <PageSection
           title="Resoluções Rápidas e Pontuais"
           subtitle="Direcione sua demanda para quem entende do assunto"
         />
       </div>
-      <div className="flex flex-grow items-center justify-center h-screen">
+      <div className="flex items-center justify-center h-screen">
         <PageSection
           title="Pare de Perder Clientes"
           subtitle="Muitos clientes desistem de uma solução por nunca conseguirem suporte adequado"
