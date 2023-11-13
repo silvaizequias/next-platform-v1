b81a404f331eb11a13a47ee8a55d18b5085c5ea2
Izequias Silva <silvaizequias@icloud.com>
2023-11-13 15:30:37 -0300

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import { Roboto } from 'next/font/google'
 import './globals.css'
 import ToastProvider from '@/components/toast-provider'
-import TopBar from '@/components/topbar'
+import AppBar from '@/components/appbar'
 
 const roboto = Roboto({
   subsets: ['latin'],
@@ -29,9 +29,9 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className={`${roboto.variable} font-sans dark`}>
+    <html lang="en" className={`${roboto.variable} font-sans`}>
       <body className=" bg-slate-200 text-slate-800 dark:bg-slate-800 dark:text-slate-200 text-base font-light">
-        <TopBar />
+        <AppBar />
         <main className="flex min-h-screen">{children}</main>
         <ToastProvider />
       </body>
