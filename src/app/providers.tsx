bd433219c3150297e4ee7060d436c9c9083079f9
Izequias Silva <silvaizequias@icloud.com>
2023-11-14 09:43:53 -0300

    analytics implemented

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,13 +2,17 @@
 
 import ToastProvider from '@/components/toast-provider'
 import { NextUIProvider } from '@nextui-org/react'
+import { Analytics } from '@vercel/analytics/react'
 import { ReactNode } from 'react'
 
 export default function Providers({ children }: { children: ReactNode }) {
+  const isDevelopment = process.env.NODE_ENV === 'development'
+
   return (
     <NextUIProvider>
       {children}
       <ToastProvider />
+      {!isDevelopment && <Analytics />}
     </NextUIProvider>
   )
 }
