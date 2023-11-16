3b5ce408a8a497d3ce566d9c82c153bebf57a451
Izequias Silva <silvaizequias@icloud.com>
2023-11-16 16:03:13 -0300

    update

--- src/app/suporte/correcao/views/index.tsx
+++ src/app/suporte/correcao/views/index.tsx
@@ -6,29 +6,29 @@ export default function CorrectionSupportView() {
       <PageSection
         title="Correção de falhas"
         subtitle="Resolva definitivamente as falhas e vulnerabilidades de sua aplicação"
-        description="Nosso suporte especializado atua em demandas que exigem um elevado nível de conhecimento de causa. Nossa ação não visa apenas corrigir os erros, mas prevenir as possíveis falhas e vulnerabilidades futuras."
+        description="Nosso suporte especializado atua em demandas que exigem um elevado nível de conhecimento de causa. Nossa ação não visa apenas corrigir os erros, mas prevenir as possíveis falhas e vulnerabilidades da aplicação."
       >
         <div className="flex justify-center mx-2 sm:mx-8">
           <div className="mx-2 sm:mx-40 md:mx-60 space-y-4">
             <p className="mx-auto text-justify">
-              Identificar e resolver rapidamente as falhas da aplicação é um dos
-              principais desafios de nosso suporte.
+              Identificar e resolver rapidamente as falhas é um dos principais
+              desafios de nosso suporte.
             </p>
             <p className="mx-auto text-justify">
-              Para um negócio ter sucesso não basta apenas garantir a presença
-              online, mas tornar o ambiente do negócio seguro para seus
-              usuários.
+              Para o negócio ter sucesso não basta apenas ter presença online.
+              Parte desse sucesso se consiste em garantir que a aplicação do
+              negócio esteja segura e livre de falhas para seus usuários.
             </p>
             <p className="mx-auto text-justify">
               A atuação da DEDICADO possibilita identificar a causa raiz do
-              problema, e traz a resposta precisa do que realmente é necessário
-              fazer para resolver esse problema, de forma estratégica e
-              profissional.
+              problema, e traz um diagnóstico preciso do que realmente é necessário
+              fazer para resolver esse problema de forma estratégica e
+              pontual.
             </p>
             <h4 className="text-center text-xl uppercase font-semibold bg-clip-text text-transparent bg-gradient-to-r from-indigo-400 to-blue-400">
               Faça contato através do chat para que possamos mapear sua
-              necessidade e oferecer o melhor plano para Correção e prevenção de
-              falhas, tudo de forma personalizada!
+              necessidade e oferecer o melhor plano para correção e prevenção de
+              falhas.
             </h4>
           </div>
         </div>
