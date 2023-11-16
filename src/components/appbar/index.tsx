c9cd0b12159ac2ec44edf550e4244c8748f2a483
Izequias Silva <silvaizequias@icloud.com>
2023-11-16 08:50:01 -0300

    update

--- src/components/appbar/index.tsx
+++ src/components/appbar/index.tsx
@@ -5,8 +5,8 @@ import TopMenu from '../topmenu'
 
 export default function AppBar() {
   return (
-    <div className="fixed w-full">
-      <div className="mx-auto py-4">
+    <div className="fixed w-full backdrop-blur-sm bg-slate/30 dark:bg-slate-800/30 shadow-md">
+      <div className="mx-auto py-2">
         <div className="flex justify-between sm:mx-8 mx-2">
           <div className="flex items-center gap-4">
             <TopMenu />
