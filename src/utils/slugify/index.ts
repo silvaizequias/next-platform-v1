f1937ffa516edce7497c63ad22ead237878a845c
Izequias Silva <silvaizequias@icloud.com>
2023-11-19 11:57:23 -0300

    implemented

---
+++ src/utils/slugify/index.ts
@@ -0,0 +1,26 @@
+export const slugify = (phrase: string) => {
+  const slug = phrase
+    .toString()
+    .toLowerCase()
+    .replace(/[àÀáÁâÂãäÄÅåª]+/g, 'a')
+    .replace(/[èÈéÉêÊëË]+/g, 'e')
+    .replace(/[ìÌíÍîÎïÏ]+/g, 'i')
+    .replace(/[òÒóÓôÔõÕöÖº]+/g, 'o')
+    .replace(/[ùÙúÚûÛüÜ]+/g, 'u')
+    .replace(/[ýÝÿŸ]+/g, 'y')
+    .replace(/[ñÑ]+/g, 'n')
+    .replace(/[çÇ]+/g, 'c')
+    .replace(/[ß]+/g, 'ss')
+    .replace(/[Ææ]+/g, 'ae')
+    .replace(/[Øøœ]+/g, 'oe')
+    .replace(/[%]+/g, 'pct')
+    .replace(/\s+/g, '-')
+    .replace(/[^\w\-]+/g, '')
+    .replace(/\-\-+/g, '-')
+    .replace(/^-+/, '')
+    .replace(/-+$/, '')
+
+  console.log(slug)
+
+  return slug
+}
