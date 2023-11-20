c10c62f795b604f9a362286599d64a7280c33df9
Izequias Silva <silvaizequias@icloud.com>
2023-11-20 15:20:29 -0300

    balancer provider implemented

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,6 +2,7 @@
 
 import CrispChat from '@/components/crisp-chat'
 import ToastProvider from '@/components/toast-provider'
+import { Provider as BalancerProvider } from 'react-wrap-balancer'
 import { NextUIProvider } from '@nextui-org/react'
 import { Analytics } from '@vercel/analytics/react'
 import { Fragment, ReactNode } from 'react'
@@ -11,7 +12,7 @@ export default function Providers({ children }: { children: ReactNode }) {
 
   return (
     <NextUIProvider>
-      {children}
+      <BalancerProvider>{children}</BalancerProvider>
       <ToastProvider />
       {!isDevelopment && (
         <Fragment>
