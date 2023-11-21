1afd7bbc47986ed78f2c68bb0814900e1f21b0ee
Izequias Silva <silvaizequias@icloud.com>
2023-11-21 11:53:07 -0300

    initial implementation

--- src/libraries/next-auth/index.ts
+++ src/libraries/next-auth/index.ts
@@ -1,5 +1,42 @@
+import { prisma } from '@/libraries/prisma'
+import { PrismaAdapter } from '@auth/prisma-adapter'
 import { NextAuthOptions } from 'next-auth'
+import { Adapter } from 'next-auth/adapters'
+import CredentialsProvider from 'next-auth/providers/credentials'
+import GoogleProvider from 'next-auth/providers/google'
+
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID!
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET!
+const URL = process.env.NEXTAUTH_URL!
+const SECRET = process.env.NEXTAUTH_SECRET!
 
 export const authOptions: NextAuthOptions = {
-  providers: [],
+  secret: SECRET,
+  adapter: PrismaAdapter(prisma) as Adapter,
+  providers: [
+    GoogleProvider({
+      clientId: GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_CLIENT_SECRET,
+    }),
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        email: { type: 'email' },
+        password: { type: 'password' },
+      },
+      async authorize(credentials, req) {
+        const res = await fetch(`${URL}/api/sign-in`, {
+          method: 'POST',
+          body: JSON.stringify(credentials),
+          headers: { 'Content-Type': 'application/json' },
+        })
+        const user = await res.json()
+
+        if (res.ok && user) {
+          return user
+        }
+        return null
+      },
+    }),
+  ],
 }
