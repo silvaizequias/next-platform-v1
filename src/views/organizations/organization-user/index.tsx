7a369cc2a6f0c023244d25b11145d821cf0e8252
Izequias Silva <silvaizequias@icloud.com>
2023-11-22 11:12:22 -0300

    initial structure

--- src/views/organizations/organization-user/index.tsx
+++ src/views/organizations/organization-user/index.tsx
@@ -1,17 +1,18 @@
 import Container from '@/components/container'
 import { Session } from 'next-auth'
-import OrganizationUser from '../organizations/organization-user'
 
 interface Props {
   session: Session
 }
 
-export default function AccountView(props: Props) {
+export default function OrganizationUser(props: Props) {
   const { session } = props
 
   return (
     <Container>
-      <OrganizationUser session={session} />
+      <h4 className="text-center font-medium text-xl sm:text-2xl uppercase">
+        Minhas Organizações
+      </h4>
     </Container>
   )
 }
