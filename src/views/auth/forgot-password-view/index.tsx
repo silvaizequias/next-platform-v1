784f10a627c81114ea5816bf771a8403a2fe4a77
Izequias Silva <silvaizequias@icloud.com>
2023-11-22 13:23:38 -0300

    refactoring

--- src/views/auth/forgot-password-view/index.tsx
+++ src/views/auth/forgot-password-view/index.tsx
@@ -1,11 +1,17 @@
 import { Card, CardBody } from '@nextui-org/react'
 import ForgotPasswordForm from '@/components/forms/auth/forgot-password'
+import PageSection from '@/components/page-section'
+import Box from '@/components/box'
 
 export default function ForgotPasswordView() {
   return (
-    <div className="min-h-screen flex justify-center items-center">
-      <div className="sm:mx-8 mx-2">
-        <div className="flex flex-col text-center">
+    <PageSection
+      title="Dedicado"
+      subtitle="Redefinição de senha"
+      description="Informe seu e-mail e telefone para receber uma nova senha de acesso"
+    >
+      <Box>
+        <div className="flex justify-center text-center">
           <Card
             className="min-w-[360px] sm:w-[400px] bg-slate-100 dark:bg-slate-900"
             isBlurred={true}
@@ -16,7 +22,7 @@ export default function ForgotPasswordView() {
             </CardBody>
           </Card>
         </div>
-      </div>
-    </div>
+      </Box>
+    </PageSection>
   )
 }
--- src/views/auth/sign-in-view/index.tsx
+++ src/views/auth/sign-in-view/index.tsx
@@ -1,18 +1,24 @@
 import { Card, CardBody } from '@nextui-org/react'
 import SignUpForm from '@/components/forms/auth/sign-up'
+import PageSection from '@/components/page-section'
+import Box from '@/components/box'
 
 export default function SignInView() {
   return (
-    <div className="min-h-screen flex justify-center items-center">
-      <div className="sm:mx-8 mx-2">
-        <div className="flex flex-col text-center">
-          <Card className="min-w-[360px] sm:w-[400px] bg-slate-100 dark:bg-slate-900" isBlurred={true} shadow='md'>
+    <PageSection title='Dedicado' subtitle='Registro de Acesso' description='Registre-se na DEDICADO para utilizar todos os serviços da plataforma'>
+      <Box>
+        <div className="flex justify-center text-center">
+          <Card
+            className="min-w-[360px] sm:w-[400px] bg-slate-100 dark:bg-slate-900"
+            isBlurred={true}
+            shadow="md"
+          >
             <CardBody>
               <SignUpForm />
             </CardBody>
           </Card>
         </div>
-      </div>
-    </div>
+      </Box>
+    </PageSection>
   )
 }
