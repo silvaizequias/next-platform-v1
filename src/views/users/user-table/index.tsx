00b51d021e17b71c73864ab532ea9707ae3ce08b
Izequias Silva <silvaizequias@icloud.com>
2023-11-21 23:44:52 -0300

    initial structured

--- src/views/users/user-table/index.tsx
+++ src/views/users/user-table/index.tsx
@@ -1,9 +1,11 @@
 'use client'
 
+import Loading from '@/app/loading'
 import useFetch from '@/hooks/use-fetch'
+import { Suspense } from 'react'
 
 export default function UserTable() {
   const { data: users } = useFetch('/api/users')
 
-  return JSON.stringify(users)
+  return <Suspense fallback={<Loading />}>{JSON.stringify(users)}</Suspense>
 }
