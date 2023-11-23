6fc08b14f53d806cc3bd0f5f151c0b59d737159c
Izequias Silva <silvaizequias@icloud.com>
2023-11-23 11:26:51 -0300

    fix

--- src/utils/send-message/templates/index.ts
+++ src/utils/send-message/templates/index.ts
@@ -5,21 +5,19 @@ export type SendTemplateType = {
   solution?: string
 }
 
-const NEXTAUTH_URL = process.env.NEXTAUTH_URL
-
 export const welcomeMessageTemplate = async (data: SendTemplateType) => {
   return `<p>Bem vindo(a) <strong>${data?.name}</strong> a ${
     data?.organization || 'DEDICADO'
   }!</p>
   <p>Esta é sua senha <strong>${
     data?.password
-  }</strong> para acessar a plataforma ${data?.solution || NEXTAUTH_URL}</p>
+  }</strong> para acessar a plataforma ${data?.solution}</p>
   <p>É um prazer ter você em nosso sistema!</p>`
 }
 
 export const passwordResetTemplate = async (data: SendTemplateType) => {
   return `<p>Olá <strong>${data?.name}</strong>!</p>
-  <p>Você está recebendo esta mensagem porque foi soliciata uma redefinição de senha para acesso a plataforma ${data?.solution || NEXTAUTH_URL} da <strong>${
+  <p>Você está recebendo esta mensagem porque foi soliciata uma redefinição de senha para acesso a plataforma ${data?.solution} da <strong>${
     data?.organization || 'DEDICADO'
   }</strong></p>
   <p>Sua nova senha é <strong>${data?.password}</strong></p>
