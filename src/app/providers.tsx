7e4a3c0509db1565d4c5ef3fe414435ea0d78047
Izequias Silva <silvaizequias@icloud.com>
2023-12-15 16:34:13 -0300

    analytics implemented

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,9 +1,12 @@
 'use client'
 
+import { Analytics } from '@vercel/analytics/react'
 import { ReactNode } from 'react'
 import { Toaster } from 'react-hot-toast'
 
 export function Providers({ children }: { children: ReactNode }) {
+  const isDevelopment = process.env.NODE_ENV === 'development'
+
   return (
     <div>
       {children}
@@ -11,6 +14,7 @@ export function Providers({ children }: { children: ReactNode }) {
         position={'top-center'}
         toastOptions={{ className: 'react-hot-toast' }}
       />
+      {!isDevelopment && <Analytics />}
     </div>
   )
 }
