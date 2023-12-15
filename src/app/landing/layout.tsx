d33025969f3eac8a398b31a5f7539271591912eb
Izequias Silva <silvaizequias@icloud.com>
2023-12-15 16:32:17 -0300

    crisp chat implemented

--- src/app/landing/layout.tsx
+++ src/app/landing/layout.tsx
@@ -1,5 +1,13 @@
+import CrispChat from '@/components/crisp-chat'
 import { ReactNode } from 'react'
 
 export default function LandingLayout({ children }: { children: ReactNode }) {
-  return <div>{children}</div>
+  const isDevelopment = process.env.NODE_ENV === 'development'
+
+  return (
+    <div>
+      {children}
+      {!isDevelopment && <CrispChat />}
+    </div>
+  )
 }
