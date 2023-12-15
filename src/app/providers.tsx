e62e236b878d9a5089a71923e611a66d949d9cf1
Izequias Silva <silvaizequias@icloud.com>
2023-12-15 16:41:01 -0300

    session and balance provider implemented

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,20 +1,22 @@
 'use client'
 
 import { Analytics } from '@vercel/analytics/react'
+import { Provider as BalancerProvider } from 'react-wrap-balancer'
 import { ReactNode } from 'react'
 import { Toaster } from 'react-hot-toast'
+import { SessionProvider } from 'next-auth/react'
 
 export function Providers({ children }: { children: ReactNode }) {
   const isDevelopment = process.env.NODE_ENV === 'development'
 
   return (
-    <div>
-      {children}
+    <SessionProvider>
+      <BalancerProvider>{children}</BalancerProvider>
       <Toaster
         position={'top-center'}
         toastOptions={{ className: 'react-hot-toast' }}
       />
       {!isDevelopment && <Analytics />}
-    </div>
+    </SessionProvider>
   )
 }
