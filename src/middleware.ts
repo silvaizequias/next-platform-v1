db5b5e482626c48bdc3b7f6960a04ef6cc220a97
Izequias Silva <silvaizequias@icloud.com>
2023-12-15 08:40:11 -0300

    implementation

---
+++ src/middleware.ts
@@ -0,0 +1,30 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+export const config = {
+  matcher: ['/((?!api/|_next/|_static/|_vercel|[\\w-]+\\.\\w+).*)'],
+}
+
+export default async function middleware(req: NextRequest) {
+  const url = req.nextUrl
+
+  let hostname = req.headers
+    .get('host')!
+    .replace('.localhost:3000', `.${process.env.NEXTAUTH_URL}`)
+
+  if (hostname.includes('---')) {
+    hostname = `${hostname.split('---')[0]}.${process.env.NEXTAUTH_URL}`
+  }
+
+  const searchParams = req.nextUrl.searchParams.toString()
+  const path = `${url.pathname}${
+    searchParams.length > 0 ? `?${searchParams}` : ''
+  }`
+
+  if (hostname === 'localhost:3000' || hostname === process.env.NEXTAUTH_URL) {
+    return NextResponse.rewrite(
+      new URL(`/${path === '/' ? '' : path}`, req.url),
+    )
+  }
+
+  return NextResponse.rewrite(new URL(`/${hostname}${path}`, req.url))
+}
