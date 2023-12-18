782069048024a4921abafd50e97f1e45a64c1209
Izequias Silva <master@dedicado.digital>
2023-12-18 17:59:23 -0300

    update

--- src/components/avatar/index.tsx
+++ src/components/avatar/index.tsx
@@ -4,21 +4,24 @@ import Image from 'next/image'
 interface Props {
   image?: string
   session?: Session
+  size?: number
 }
 
 export default function Avatar(props: Props) {
-  const { image, session } = props
+  const { image, session, size } = props
   const avatar = '/avatar.svg'
 
   return (
     <div className="rounded-full p-2 hover:opacity-75 cursor-pointer">
       <Image
-        className="w-[28px] sm:w-[32px] h-[28px] sm:h-[32px]"
+        className={`w-[${size || 28}px] sm:w-[${size || 32}px] h-[${
+          size || 28
+        }px] sm:h-[${size || 32}px]`}
         src={image || session?.user?.image || avatar}
         alt={session?.user?.name || 'user avatar'}
         priority
-        width={32}
-        height={32}
+        width={size || 32}
+        height={size || 32}
       />
     </div>
   )
