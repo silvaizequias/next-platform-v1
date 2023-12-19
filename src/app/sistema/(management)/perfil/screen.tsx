f488cd2ba7c2550255590b9995be36042e3f2d82
Izequias Silva <master@dedicado.digital>
2023-12-19 17:12:43 -0300

    refactoring

--- src/app/sistema/(management)/perfil/screen.tsx
+++ src/app/sistema/(management)/perfil/screen.tsx
@@ -11,10 +11,16 @@ export default function ProfileScreen() {
     <div className="flex flex-col md:flex-row gap-4">
       <div className="min-w-220 sm:max-w-260 md:max-w-[320px] w-full p-4">
         <div className="flex justify-center">
-          <div className="flex flex-col justify-center items-center gap-2">
-            <Avatar image={profile?.image} size={110} />
-            <h4 className="text-xl text-slate-400 font-medium">{profile?.name}</h4>
-            <span className="bg-slate-50 text-slate-400 rounded-md py-2 px-4 text-xs">
+          <div className="flex flex-col justify-center items-center gap-4">
+            <div className="flex justify-center item-center mb-2">
+              <div className="p-10 rounded-full bg-slate-50 border-4 border-sky-600 shadow-lg">
+                <Avatar image={profile?.image} size={160} />
+              </div>
+            </div>
+            <h4 className="text-xl sm:text-4xl text-slate-400 font-medium">
+              {profile?.name.split(' ')[0]}
+            </h4>
+            <span className="bg-slate-50 text-slate-400 rounded-md shadow-md py-2 px-4 text-xs">
               {profile?.profile}
             </span>
           </div>
