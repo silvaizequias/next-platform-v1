bde24599e8cae3d9194eaa2625f970a06d280ddb
Izequias Silva <master@dedicado.digital>
2023-12-19 16:34:51 -0300

    update

--- src/components/app-bar/index.tsx
+++ src/components/app-bar/index.tsx
@@ -23,17 +23,20 @@ export default function AppBar(props: Props) {
   )
 
   return (
-    <div className="flex items-center justify-end">
-      <div className="rounded-full p-2 hover:opacity-75 cursor-pointer">
+    <div className="flex items-center justify-end gap-2">
+      <div className="rounded-full p-2 hover:bg-slate-100 cursor-pointer">
         <div
-          className="flex items-center justify-center w-[28px] sm:w-[32px] h-[28px] sm:h-[32px] text-slate-100 hover:text-sky-400 text-2xl sm:text-4xl"
+          className="flex items-center justify-center w-[24px] sm:w-[28px] h-[24px] sm:h-[28px] text-sky-400 text-xl sm:text-2xl"
           onClick={() => handleClick('https://sistema.dedicado.digital')}
         >
           <MdDashboard />
         </div>
       </div>
       {session && (
-        <div className="block" onClick={() => handleClick('/perfil')}>
+        <div
+          className="rounded-full p-2 hover:bg-slate-100 cursor-pointer"
+          onClick={() => handleClick('/perfil')}
+        >
           <Avatar session={session} />
         </div>
       )}
