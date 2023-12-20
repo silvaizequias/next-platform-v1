028b33b56dc43f8399e73eed51b74093ff34b8fe
Izequias Silva <master@dedicado.digital>
2023-12-20 11:24:43 -0300

    update

--- src/app/sistema/(management)/blog/views/PostListView.tsx
+++ src/app/sistema/(management)/blog/views/PostListView.tsx
@@ -6,11 +6,5 @@ import { PostType } from '@/types/post'
 export default function PostListView() {
   const { data: posts } = useFetch<PostType[] | any>('/api/posts')
 
-  return posts ? (
-    JSON.stringify(posts)
-  ) : (
-    <h6 className="text-lg sm:text-xl text-center lowercase">
-      Sem postagens para exibir!
-    </h6>
-  )
+  return JSON.stringify(posts)
 }
