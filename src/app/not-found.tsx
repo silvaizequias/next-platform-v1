3e019d5ba96e836f52d48c8f804350f2912c461a
Izequias Silva <master@dedicado.digital>
2023-12-21 07:08:10 -0300

    refactoring

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,15 +1,28 @@
-import Link from 'next/link'
+'use client'
 
-export default async function NotFound() {
+import Link from 'next/link'
+import { useRouter } from 'next/navigation'
+
+export default function NotFound() {
+  const router = useRouter()
+
   return (
     <div className="min-h-screen flex justify-center items-center">
       <div className="sm:mx-8 mx-2">
-        <div className="flex flex-col text-center uppercase">
-          <h2 className="font-medium text-6xl">Ops!</h2>
-          <p className="font-light text-base">
+        <div className="flex flex-col justify-center gap-4">
+          <h4 className="font-semibold text-center text-red-400 text-4xl sm:text-6xl">
+            Ops!{' '}
+          </h4>
+          <p className="font-light text-base text-center">
             O conteúdo não foi encontrado...
           </p>
-          <Link className='font-thin text-xs hover:opacity-50' href="/">Retorne ao Início</Link>
+          <button
+            className="mt-2 w-full uppercase rounded-md bg-sky-600 hover:opacity-75 py-2 text-white text-base hover:font-medium"
+            type="button"
+            onClick={() => router.back()}
+          >
+            Retorne daqui
+          </button>
         </div>
       </div>
     </div>
