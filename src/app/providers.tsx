96a6f4ba9ee821772a9fbb4ce57f45ef8ba7269d
Izequias Silva <master@dedicado.digital>
2023-12-22 18:03:48 -0300

    material tailwind implemented

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -5,17 +5,20 @@ import { Provider as BalancerProvider } from 'react-wrap-balancer'
 import { ReactNode } from 'react'
 import { Toaster } from 'react-hot-toast'
 import { SessionProvider } from 'next-auth/react'
+import { ThemeProvider } from '@material-tailwind/react'
 
 export function Providers({ children }: { children: ReactNode }) {
   const isDevelopment = process.env.NODE_ENV === 'development'
 
   return (
     <SessionProvider>
-      <BalancerProvider>{children}</BalancerProvider>
-      <Toaster
-        position={'top-center'}
-        toastOptions={{ className: 'react-hot-toast' }}
-      />
+      <ThemeProvider>
+        <BalancerProvider>{children}</BalancerProvider>
+        <Toaster
+          position={'top-center'}
+          toastOptions={{ className: 'react-hot-toast' }}
+        />
+      </ThemeProvider>
       {!isDevelopment && <Analytics />}
     </SessionProvider>
   )
