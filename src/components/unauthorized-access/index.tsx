fad51b567ea1c1082ded2ff242f48c37dec788d3
Izequias Silva <master@dedicado.digital>
2023-12-23 20:07:00 -0300

    update

--- src/components/unauthorized-access/index.tsx
+++ src/components/unauthorized-access/index.tsx
@@ -1,14 +1,24 @@
-import ButtonWithAction from '@/components/button-with-action'
+'use client'
+
 import PageSection from '@/components/page-section'
+import { useRouter } from 'next/navigation'
 
 export default function UnauthorizedAccess() {
+  const router = useRouter()
+
   return (
     <PageSection
       subtitle="Acesso não autorizado!"
       description="Você não tem permissão para acessar esta página"
     >
       <div className="flex justify-center">
-        <ButtonWithAction name="Ir para o início" path={'/'} />
+        <button
+          className="mt-2 w-full uppercase rounded-md bg-sky-600 hover:opacity-75 py-2 text-white text-base hover:font-medium"
+          type="button"
+          onClick={() => router.back()}
+        >
+          Retorne daqui
+        </button>
       </div>
     </PageSection>
   )
