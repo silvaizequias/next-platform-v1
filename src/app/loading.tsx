79ea52270b59208656a1dae185e64a6be7adaa75
Izequias Silva <master@dedicado.digital>
2023-12-23 20:10:19 -0300

    update

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,8 +1,12 @@
+import { Spinner } from '@material-tailwind/react'
+
 export default function Loading() {
   return (
     <div className="min-h-screen flex justify-center items-center">
       <div className="sm:mx-8 mx-2">
-        <div className="flex flex-col text-center">...</div>
+        <div className="flex flex-col text-center">
+          <Spinner color="blue" className="h-12 w-12" />
+        </div>
       </div>
     </div>
   )
