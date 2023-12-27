4d7dae00962dcfc655d6e79093a39f38f4209255
Izequias Silva <master@dedicado.digital>
2023-12-26 21:52:06 -0300

    editor implemented

--- src/app/sistema/(management)/blog/views/PostEditor.tsx
+++ src/app/sistema/(management)/blog/views/PostEditor.tsx
@@ -2,14 +2,63 @@
 
 import { Button } from '@material-tailwind/react'
 import { Controller, SubmitHandler, useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Editor } from 'novel'
+import { useCallback, useState } from 'react'
+import { CreatePostDTO, CreatePostDTOType } from '@/app/api/posts/dto'
+import { PostType } from '@/types/post'
+import useFetch from '@/hooks/use-fetch'
+import toast from 'react-hot-toast'
 
 export default function PostEditor() {
-  const { control, handleSubmit, register } = useForm<any>({
-    defaultValues: { content: '...' },
+  const { data: posts, mutate } = useFetch<PostType[] | any>('/api/posts')
+
+  const [content, setContent] = useState<string | undefined>('')
+
+  const handleContent = useCallback((content: any) => {
+    content && setContent(content)
+  }, [])
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+    register,
+    reset,
+  } = useForm<CreatePostDTOType>({
+    mode: 'all',
+    resolver: zodResolver(CreatePostDTO),
+    defaultValues: {
+      content: content,
+      title: 'Conteúdo para teste',
+      subject: 'Isso é apenas um conteúdo de teste',
+    },
   })
 
-  const onSubmit: SubmitHandler<any> = async (inputs: { content?: string }) => {
-    alert(inputs.content)
+  const onSubmit: SubmitHandler<CreatePostDTOType> = async (inputs) => {
+    try {
+      await fetch(`/api/posts`, {
+        method: 'POST',
+        body: JSON.stringify(inputs),
+        headers: { 'Content-Type': 'application/json' },
+      }).then(async (res: any) => {
+        const data = await res.json()
+        if (res.status == 201) {
+          await mutate(...posts, data, {
+            revalidate: true,
+            rollbackOnError: true,
+          })
+          toast.success(data)
+        } else {
+          toast.error(data)
+        }
+      })
+    } catch (error: any) {
+      toast.error(error?.message)
+      console.error(error)
+    } finally {
+      reset(inputs)
+    }
   }
 
   return (
@@ -17,24 +66,25 @@ export default function PostEditor() {
       onSubmit={handleSubmit(onSubmit)}
       className="flex flex-col flex-1 gap-4 m-2 sm:min-w-[400px]"
     >
-      <div className="bg-gray-50 border border-spacing-2 rounded-md shadow-md">
-        <Controller
-          {...register('content')}
-          control={control}
-          render={({ field: { value, onChange } }) => (
-            <input className='w-full border-none' value={value} onChange={onChange} />
-          )}
+      <div className="relative block">
+        <Editor
+          defaultValue={content}
+          onUpdate={(editor) => {
+            handleContent(editor?.getHTML())
+          }}
         />
       </div>
-      <Button
-        variant="gradient"
-        color="green"
-        size="sm"
-        fullWidth
-        type="submit"
-      >
-        Criar Postagem
-      </Button>
+      <div className="relative bg-green-400 rounded-md">
+        <Button
+          variant="gradient"
+          color="green"
+          size="sm"
+          fullWidth
+          type="submit"
+        >
+          Criar Postagem
+        </Button>
+      </div>
     </form>
   )
 }
