b69f412d9a20e472fa62a2354c767b1b81de5906
Izequias Silva <master@dedicado.digital>
2023-12-31 13:26:26 -0300

    refactored

--- src/components/app-bar/index.tsx
+++ src/components/app-bar/index.tsx
@@ -2,9 +2,8 @@
 
 import { Session } from 'next-auth'
 import { MdDashboard, MdOutlineArticle } from 'react-icons/md'
-import { useRouter } from 'next/navigation'
-import { useCallback } from 'react'
 import { Avatar, Tooltip } from '@material-tailwind/react'
+import Link from 'next/link'
 
 interface Props {
   session: Session
@@ -18,59 +17,46 @@ export default function AppBar(props: Props) {
   const isDevelopment = process.env.NODE_ENV === 'development'
   const NEXT_PUBLIC_URL = process.env.NEXT_PUBLIC_URL!
 
-  const router = useRouter()
-
-  const handleClick = useCallback(
-    (path?: string) => {
-      if (!path) {
-        if (!isDevelopment) router.push(`https://sistema.${NEXT_PUBLIC_URL}`)
-
-        router.push(`http://sistema.${NEXT_PUBLIC_URL}`)
-      }
-
-      path && router.push(path)
-    },
-    [NEXT_PUBLIC_URL, isDevelopment, router],
-  )
-
   return (
     <div className="flex items-center justify-end gap-2">
       <Tooltip content={'blog dedicado'}>
-        <div className="rounded-full p-2 hover:bg-gray-50 cursor-pointer">
-          <div
-            className="flex items-center justify-center w-[24px] sm:w-[28px] h-[24px] sm:h-[28px] text-light-blue-200 hover:text-light-blue-400 text-xl sm:text-2xl"
-            onClick={() =>
-              handleClick(
-                !isDevelopment
-                  ? `https://blog.${NEXT_PUBLIC_URL}`
-                  : `http://blog.${NEXT_PUBLIC_URL}`,
-              )
-            }
-          >
+        <Link
+          href={
+            !isDevelopment
+              ? `https://blog.${NEXT_PUBLIC_URL}`
+              : `http://blog.${NEXT_PUBLIC_URL}`
+          }
+          className="rounded-full p-2 hover:bg-gray-50 cursor-pointer"
+        >
+          <div className="flex items-center justify-center w-[24px] sm:w-[28px] h-[24px] sm:h-[28px] text-light-blue-200 hover:text-light-blue-400 text-xl sm:text-2xl">
             <MdOutlineArticle />
           </div>
-        </div>
+        </Link>
       </Tooltip>
 
       <Tooltip content={'sistema dedicado'}>
-        <div className="rounded-full p-2 hover:bg-gray-50 cursor-pointer">
-          <div
-            className="flex items-center justify-center w-[24px] sm:w-[28px] h-[24px] sm:h-[28px] text-light-blue-200 hover:text-light-blue-400 text-xl sm:text-2xl"
-            onClick={() => handleClick()}
-          >
+        <Link
+          href={
+            !isDevelopment
+              ? `https://sistema.${NEXT_PUBLIC_URL}`
+              : `http://sistema.${NEXT_PUBLIC_URL}`
+          }
+          className="rounded-full p-2 hover:bg-gray-50 cursor-pointer"
+        >
+          <div className="flex items-center justify-center w-[24px] sm:w-[28px] h-[24px] sm:h-[28px] text-light-blue-200 hover:text-light-blue-400 text-xl sm:text-2xl">
             <MdDashboard />
           </div>
-        </div>
+        </Link>
       </Tooltip>
 
       {session && (
         <Tooltip content={'detalhes do perfil'}>
-          <div
+          <Link
+            href={'/perfil'}
             className="rounded-full p-2 opacity-25 hover:opacity-95 hover:bg-gray-50   cursor-pointer"
-            onClick={() => handleClick('/perfil')}
           >
             <Avatar size="xs" src={session?.user?.image || avatar} />
-          </div>
+          </Link>
         </Tooltip>
       )}
     </div>
