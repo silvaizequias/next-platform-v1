9366e5c0e72bef33d5044598c16478f6d6561d74
Izequias Silva <master@dedicado.digital>
2024-01-03 14:22:35 -0300

    update

--- src/utils/get-data-by-params/index.ts
+++ src/utils/get-data-by-params/index.ts
@@ -1,10 +1,30 @@
+import { Session } from 'next-auth'
+
 const NEXTAUTH_URL = process.env.NEXTAUTH_URL!
+const PLATFORM_API_URL = process.env.PLATFORM_API_URL!
 
 export const getPostByParams = async (slug: string) => {
+  if (!slug) return null
+
   const data = await fetch(`${NEXTAUTH_URL}/api/posts/slug/${slug}`, {
     method: 'GET',
     headers: { 'Content-Type': 'application/json' },
-    next: { revalidate: 3600 }
+    next: { revalidate: 3600 },
+  })
+  return data && data.json()
+}
+
+export const getProfileBySession = async (session: Session) => {
+  if (!session) return null
+
+  const data = await fetch(`${PLATFORM_API_URL}/users/${session.user.id}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+      Authorization: `Bearer ${session.user.authorization}`,
+    },
+    next: { revalidate: 3600 },
   })
   return data && data.json()
 }
