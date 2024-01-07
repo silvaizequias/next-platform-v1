8e472349c217f945b66bf3ddbc3d19a62387a9fb
Izequias Silva <master@dedicado.digital>
2024-01-07 02:59:59 -0300

    refactoring

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -4,9 +4,10 @@ import { DefaultSession } from 'next-auth'
 declare module 'next-auth/jwt' {
   interface JWT {
     id: string
-    profile: 'USER' | 'MASTER'
-    authorization: string
-    organizations: [] | null
+    profile: string
+    active: boolean
+    subscriber: boolean
+    phone: string
   }
 }
 
@@ -14,16 +15,18 @@ declare module 'next-auth' {
   interface Session {
     user: User & {
       id: string
-      profile: 'USER' | 'MASTER'
-      authorization: string
-      organizations: [] | null
+      profile: string
+      active: boolean
+      subscriber: boolean
+      phone: string
     } & DefaultSession['user']
   }
 
   interface User {
     id: string
-    profile: 'USER' | 'MASTER'
-    authorization: string
-    organizations: [] | null
+    profile: string
+    active: boolean
+    subscriber: boolean
+    phone: string
   }
 }
