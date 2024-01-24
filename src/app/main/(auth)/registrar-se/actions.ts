5df640b0438ebfd754457d7a7f6ded9f9226fff3
Izequias Silva <master@dedicado.digital>
2024-01-24 08:41:01 -0300

    refactored

--- src/app/main/(auth)/registrar-se/actions.ts
+++ src/app/main/(auth)/registrar-se/actions.ts
@@ -1,11 +1,41 @@
 'use server'
 
+import { prisma } from '@/libraries/prisma'
 import { SignUpDTOType } from './dto'
+import { Prisma } from '@prisma/client'
+import { hashSync } from 'bcrypt'
 
 export async function actionSignUp(inputs: SignUpDTOType): Promise<any> {
+  const randomCode = Math.random().toString(32).substr(2, 14)
   try {
-    return inputs
+    const { email, password, phone } = inputs
+    delete inputs?.password
+
+    const setPassword = password || randomCode
+
+    const userPhone = await prisma.user.findFirst({
+      where: { phone: phone },
+    })
+
+    const userEmail = await prisma.user.findFirst({
+      where: { email: email },
+    })
+
+    if (userPhone) return `o número ${phone} já existe na plataforma`
+
+    if (userEmail) return `o email ${email} já existe na plataforma`
+
+    const data: Prisma.UserCreateInput = {
+      ...inputs,
+      passHash: hashSync(setPassword, 10),
+    }
+    await prisma.user.create({ data })
+
+    return `sua conta foi criada na plataforma e esta é a seua senha de acesso: ${setPassword}`
   } catch (error: any) {
+    await prisma.$disconnect()
     throw new Error(error)
+  } finally {
+    await prisma.$disconnect()
   }
 }
