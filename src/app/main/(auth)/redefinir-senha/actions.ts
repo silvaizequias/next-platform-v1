9ddf4c5015c571eee3da4315bfd9eec027d03751
Izequias Silva <master@dedicado.digital>
2024-01-24 08:53:06 -0300

    refactored

--- src/app/main/(auth)/redefinir-senha/actions.ts
+++ src/app/main/(auth)/redefinir-senha/actions.ts
@@ -1,22 +1,62 @@
 'use server'
 
-import { ResetPasswordDTOType } from './dto'
+import { prisma } from '@/libraries/prisma'
+import { ResetPasswordCodeDTOType, ResetPasswordDTOType } from './dto'
+import { Prisma } from '@prisma/client'
+import { hashSync } from 'bcrypt'
 
 export async function actionResetPassword(
   inputs: ResetPasswordDTOType,
 ): Promise<any> {
+  const randomCode = Math.random().toString(32).substr(2, 6).toUpperCase()
   try {
-    const NEXTAUTH_URL = process.env.NEXTAUTH_URL
-    const response = await fetch(`${NEXTAUTH_URL}/api/reset-password`, {
-      method: 'POST',
-      body: JSON.stringify(inputs),
-      headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-      },
+    const { phone } = inputs
+
+    const user = await prisma.user.findFirst({
+      where: { phone: phone },
     })
-    return { status: response.status, message: response && response.json() }
+
+    if (!user)
+      return `o número ${phone} não possui registro ativo na plataforma`
+
+    const data: Prisma.UserUpdateInput = {
+      active: false,
+      accessCode: randomCode,
+    }
+    await prisma.user.update({ where: { id: user?.id }, data })
+
+    return `utilize o código ${randomCode} para redefinir a senha`
   } catch (error: any) {
+    await prisma.$disconnect()
     throw new Error(error)
+  } finally {
+    await prisma.$disconnect()
+  }
+}
+
+export async function actionWithPasswordCode(
+  inputs: ResetPasswordCodeDTOType,
+): Promise<any> {
+  const randomCode = Math.random().toString(32).substr(2, 14)
+  try {
+    const { accessCode } = inputs
+
+    const user = await prisma.user.findFirst({
+      where: { accessCode: accessCode, softDeleted: false, active: false },
+    })
+    if (!user) return `o código informado não é válido`
+
+    const data: Prisma.UserUpdateInput = {
+      active: true,
+      passHash: hashSync(randomCode, 10),
+    }
+    await prisma.user.update({ where: { id: user?.id }, data })
+
+    return `a senha foi redefinida: ${randomCode}`
+  } catch (error: any) {
+    await prisma.$disconnect()
+    throw new Error(error)
+  } finally {
+    await prisma.$disconnect()
   }
 }
