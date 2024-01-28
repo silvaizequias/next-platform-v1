921fd8c496286733a653b7ddc2b9cb27e751131b
Izequias Silva <master@dedicado.digital>
2024-01-28 17:53:58 -0300

    update

--- src/app/main/(management)/profile/actions.ts
+++ src/app/main/(management)/profile/actions.ts
@@ -4,6 +4,8 @@ import { Session } from 'next-auth'
 import {
   UpdateProfileInformationDTO,
   UpdateProfileInformationDTOType,
+  UpdateProfilePasswordDTO,
+  UpdateProfilePasswordDTOType,
 } from './dto'
 import { revalidatePath } from 'next/cache'
 
@@ -39,6 +41,33 @@ export async function actionUpdateProfile(
         {
           method: 'PATCH',
           body: JSON.stringify(inputs),
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${session?.user?.authorization}`,
+          },
+        },
+      )
+      revalidatePath('/profile')
+      return data && (await data.json())
+    }
+  } catch (error: any) {
+    console.error(error?.message || error)
+  }
+}
+
+export async function actionUpdateProfilePassword(
+  session: Session,
+  inputs: UpdateProfilePasswordDTOType,
+) {
+  try {
+    if (await UpdateProfilePasswordDTO.parseAsync(inputs)) {
+      const data = await fetch(
+        `${PLATFORM_MANAGEMENT_URL}/users/${session?.user?.id}`,
+        {
+          method: 'PATCH',
+          body: JSON.stringify({
+            password: inputs?.newPassword,
+          }),
           headers: {
             'Content-Type': 'application/json',
             Authorization: `Bearer ${session?.user?.authorization}`,
