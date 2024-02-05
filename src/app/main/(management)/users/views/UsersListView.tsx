55ceb0b56c149dd84453a9701be43fbbcea564dc
Izequias Silva <master@dedicado.digital>
2024-02-05 20:53:21 -0300

    fix

--- src/app/main/(management)/users/views/UsersListView.tsx
+++ src/app/main/(management)/users/views/UsersListView.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Card, CardContent } from '@mui/material'
+import { Box } from '@mui/material'
 import { DataGrid } from '@mui/x-data-grid'
 import { UserType } from '../types'
 import { UsersColumnsView } from './UsersColumnsView'
@@ -13,16 +13,14 @@ export default function UsersListView(props: Props) {
   const { users } = props
 
   return (
-    <Card sx={{ minWidth: '380px' }}>
-      <CardContent sx={{ width: '100%' }}>
-        <DataGrid
-          autoHeight
-          getRowId={(user) => user?.id}
-          rows={users}
-          columns={UsersColumnsView}
-          disableColumnSelector
-        />
-      </CardContent>
-    </Card>
+    <Box sx={{ maxWidth: 'md', width: '100%', padding: '10px' }}>
+      <DataGrid
+        autoHeight
+        getRowId={(user) => user?.id}
+        rows={users}
+        columns={UsersColumnsView}
+        disableColumnSelector
+      />
+    </Box>
   )
 }
