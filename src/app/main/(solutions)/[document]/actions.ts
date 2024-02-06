cf18f33849a98daf4d5cf77afb160de56df7c675
Izequias Silva <master@dedicado.digital>
2024-02-06 10:55:42 -0300

    spending count implemented

--- src/app/main/(solutions)/[document]/actions.ts
+++ src/app/main/(solutions)/[document]/actions.ts
@@ -3,6 +3,7 @@
 import { Session } from 'next-auth'
 import { OrganizationType } from '../../(management)/organizations/types'
 import { env } from '@/environments'
+import { prisma } from '@/libraries/prisma'
 
 export async function actionGetOrganizationByDocument(
   document: string,
@@ -17,12 +18,28 @@ export async function actionGetOrganizationByDocument(
           'Content-Type': 'application/json',
           Authorization: `Bearer ${session?.user?.authorization}`,
         },
-        next: { revalidate: 3600 }
+        next: { revalidate: 3600 },
       },
     )
     if (!data) return null
     return data && (await data.json())
   } catch (error: any) {
     console.error(error?.message || error)
+  }
+}
+
+export async function actionGetOrganizationApiSpend(document: string) {
+  try {
+    const spending = await prisma.apiSpend.count({
+      where: { document: document },
+    })
+    if (!spending) return 0
+
+    return spending
+  } catch (error: any) {
+    await prisma.$disconnect()
+    console.error(error?.message || error)
+  } finally {
+    await prisma.$disconnect()
   }
 }
---
+++ src/app/main/(solutions)/[document]/creditos/views/MyOrganizationSubscriptionsSpendingView.tsx
@@ -0,0 +1,31 @@
+'use client'
+
+import { Box, LinearProgress, Typography } from '@mui/material'
+import { useCallback, useState } from 'react'
+
+interface Props {
+  spending: number | 0
+}
+
+export default function MyOrganizationSubscriptionsSpendingView(props: Props) {
+  const { spending } = props
+  const [progress, setProgress] = useState(spending || 0)
+
+  useCallback(() => {
+    setProgress((spending) => {
+      if (spending === 100) {
+        return 0
+      }
+      return Math.min(spending, 100)
+    })
+  }, [])
+
+  return (
+    <Box sx={{ width: '100%', paddingBottom: '16px' }}>
+      <Typography variant="caption">
+        {`consumo de créditos da organização: ${spending}/100`}
+      </Typography>
+      <LinearProgress variant="determinate" value={progress} />
+    </Box>
+  )
+}
