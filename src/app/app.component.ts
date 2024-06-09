fb66a538a888d7f26a393b268b477d5794854d09
silvaizequias <master@dedicado.digital>
2024-06-09 12:35:34 -0300

    base: initial state

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { CommonModule } from '@angular/common'
-import { HttpClient } from '@angular/common/http'
 import { Component } from '@angular/core'
 import { RouterOutlet } from '@angular/router'
 
@@ -7,28 +6,12 @@ import { RouterOutlet } from '@angular/router'
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
-  template: ` <div>
-    <router-outlet />
-    <ul *ngFor="let user of users">
-      <li>{{ user.id }}
-      <a>{{user?.name.firstname}}</a>
-      </li>
-    </ul>
-  </div>`,
+  template: `
+    <div>
+      <router-outlet />
+    </div>
+  `,
 })
 export class AppComponent {
   title = 'platform-management'
-  private api = 'https://fakestoreapi.com/users'
-  users: any = []
-
-  constructor(private httpClient: HttpClient) {
-    this.findUsers()
-    console.log(this.users)
-  }
-
-  findUsers() {
-    return this.httpClient
-      .get(this.api)
-      .subscribe((data) => (this.users = data))
-  }
 }
