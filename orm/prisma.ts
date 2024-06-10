748572907acca60030362098f4074fe6450fd1dc
silvaizequias <master@dedicado.digital>
2024-06-10 09:30:19 -0300

    feature: prisma orm implemented

---
+++ orm/prisma.ts
@@ -0,0 +1,20 @@
+import { PrismaClient } from '@prisma/client'
+
+declare global {
+  var prisma: PrismaClient | undefined
+}
+
+export const prisma =
+  global.prisma ||
+  new PrismaClient({
+    log: process.env.NODE_ENV === 'development' ? ['error', 'warn'] : ['error'],
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL,
+      },
+    },
+  })
+
+if (process.env.NODE_ENV === 'development') {
+  global.prisma = prisma
+}
