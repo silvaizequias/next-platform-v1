c2695ce1665b052652028ca328c7080990c1ca30
Izequias Silva <master@dedicado.digital>
2024-06-23 22:58:59 -0300

    feature: initial implementations

---
+++ app/blog/page.tsx
@@ -0,0 +1,7 @@
+export default function BlogPage() {
+  return (
+    <div className="w-full h-screen flex justify-center items-center">
+      blog
+    </div>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 export default function HomePage() {
   return (
-    <div className="w-full h-screen flex justify-center items-center"></div>
+    <div className="w-full h-screen flex justify-center items-center">home</div>
   )
 }
