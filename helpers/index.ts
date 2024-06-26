96c9334b9ae6ca7191e90bd2806be58e44b15dce
Izequias Silva <master@dedicado.digital>
2024-06-26 09:10:43 -0300

    update

--- helpers/index.ts
+++ helpers/index.ts
@@ -4,8 +4,11 @@ const baseUrl = environment.baseUrl
 
 export const onLive = process.env.NODE_ENV
 
-export const platformUrl =
+export const mainUrl =
   onLive == 'production' ? `https://${baseUrl}` : `http://${baseUrl}`
 
 export const apiUrl =
   onLive == 'production' ? `https://api.${baseUrl}` : `http://api.${baseUrl}`
+
+export const blogUrl =
+  onLive == 'production' ? `https://blog.${baseUrl}` : `http://blog.${baseUrl}`
