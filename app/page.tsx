3436cd49c6a27f152580d86887e826769fda77be
Izequias Silva <master@dedicado.digital>
2024-06-26 11:43:38 -0300

    update: landing page aprimored

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,29 @@
+import { Metadata } from 'next'
 import Image from 'next/image'
-import { MdAutoGraph, MdCloudSync, MdOutlinePolyline } from 'react-icons/md'
+import {
+  MdAutoGraph,
+  MdCloudSync,
+  MdOutlineKeyboardArrowDown,
+  MdOutlinePolyline,
+} from 'react-icons/md'
+
+export const metadata: Metadata = {
+  title: {
+    default:
+      'Potencialize a capacidade da sua demanda operacional utilizando inteligência artificial',
+    template: `%s | Dedicado`,
+  },
+  description:
+    'A dedicado oferece soluções personalizadas de alta performance que aumentam a produtividade de pessoas e organizações',
+}
 
 export default function HomePage() {
   const logotipo = '/logotipo.svg'
+  const infrastructure = '/cloud_hosting_7xb1.svg'
 
   return (
     <main className="w-full h-full flex justify-center items-center">
-      <div className='w-full bg-gradient-to-b from-sky-200/60 to-slate-200'>
+      <div className="w-full bg-gradient-to-b from-sky-200/60 to-slate-200">
         <header className="w-full sm:max-w-4xl mx-auto p-2 py-24 flex flex-col sm:flex-row-reverse justify-center items-center">
           <figure className="sm:w-1/3 w-[200px] h-[350px] mx-auto flex justify-center">
             <Image
@@ -45,6 +62,10 @@ export default function HomePage() {
             a <span className="text-sky-800">produtividade</span> de pessoas e
             organizações
           </h4>
+          <MdOutlineKeyboardArrowDown
+            size={60}
+            className="mx-auto mt-4 text-sky-800 animate-bounce cursor-pointer"
+          />
         </div>
       </section>
       <section className="w-full h-full py-4">
@@ -91,8 +112,47 @@ export default function HomePage() {
           </div>
         </div>
       </section>
-      <section className="w-full h-full py-4 bg-slate-50">
-        <div className="w-full mx-auto p-8 sm:max-w-4xl"></div>
+      <section className="w-full h-full py-4 bg-gradient-to-t from-white to-slate-200">
+        <div className="w-full mx-auto p-8 sm:max-w-4xl">
+          <div className="w-full flex flex-col sm:flex-row justify-center sm:justify-between items-center gap-4">
+            <figure className="mx-auto w-auto h-auto p-4">
+              <Image
+                src={infrastructure}
+                alt="infraestrututa dedicado"
+                width={500}
+                height={490}
+                priority
+              />
+            </figure>
+            <div className="w-full flex flex-col justify-center item-center gap-4">
+              <h4 className="text-sky-800 text-center sm:text-left">
+                Infraestrutura SaaS
+              </h4>
+              <h6 className="text-slate-600 text-balance text-center sm:text-left -mt-4">
+                Garantia de alta disponibilidade, segurança escalabilidade
+              </h6>
+              <p className="w-full text-pretty text-center sm:text-left">
+                Todos os serviços de nossas soluções são alocados nas maiores
+                plataformas de computação em nuvem do mundo.
+              </p>
+              <p className="w-full text-pretty text-center sm:text-left">
+                Nossa arquitetura é envolvida por uma camada de isolamento VPC
+                que utiliza protocolos de segurança para a comunicação entre
+                serviços.
+              </p>
+              <p className="w-full text-pretty text-center sm:text-left">
+                Cada serviço de nossas soluções conta com um gerenciador de
+                balanceamento de carga para garantir a disponibilidade em
+                condições de alta demanda.
+              </p>
+              <div className="w-full flex justify-center sm:justify-start">
+                <button className="w-full max-w-sm p-2 hover:shadow-md bg-gradient-to-r from-sky-900 to-cyan-500 hover:opacity-80 rounded-md text-slate-200 font-semibold uppercase animate-pulse">
+                  Saiba mais
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
       </section>
     </main>
   )
