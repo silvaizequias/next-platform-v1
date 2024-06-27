79b28f3a51e1889c1f64b2e5488fc96005c6c5fe
Izequias Silva <master@dedicado.digital>
2024-06-27 13:14:56 -0300

    feature: support mdx file implemented

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,14 @@
+import createMDX from '@next/mdx'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  compiler: {
+    styledComponents: true,
+  },
+  pageExtensions: ['md', 'mdx', 'ts', 'tsx'],
+  experimental: {
+    mdxRs: true,
+  },
   env: {
     BASE_URL: process.env.BASE_URL ?? '',
     DATABASE_URL: process.env.DATABASE_URL ?? '',
@@ -24,4 +33,12 @@ const nextConfig = {
   },
 }
 
-export default nextConfig
+const withMDX = createMDX({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+  },
+})
+
+export default withMDX(nextConfig)
