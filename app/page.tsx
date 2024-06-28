33c7c985f7e20bdd396fe18b72ac5eacccac37a7
Izequias Silva <master@dedicado.digital>
2024-06-28 09:04:36 -0300

    update: reordenated

--- app/page.tsx
+++ app/page.tsx
@@ -77,97 +77,7 @@ export default function HomePage() {
         </div>
       </section>
 
-      <section className="w-full h-full py-4">
-        <div className="w-full mx-auto p-8 sm:max-w-4xl">
-          <h2 className="leading-tight text-center text-balance font-light text-sky-800 uppercase">
-            <span className="font-semibold">Cuidamos de tudo</span> pra você
-          </h2>
-          <div className="w-full py-8 flex flex-col sm:flex-row justify-center items-start gap-4">
-            <div className="w-full flex flex-col justify-start items-center gap-4">
-              <MdOutlinePolyline size={80} className="text-sky-400/80" />
-              <h6 className="text-sky-800 text-center font-bold uppercase">
-                Integração
-              </h6>
-              <small className="text-center text-pretty">
-                Integramos sistemas com nossas soluções de gestão de serviços
-                transacionais otimizada com inteligência artificial, e
-                desenvolvemos soluções personalizadas para sistemas já
-                existentes.
-              </small>
-            </div>
-            <div className="w-full flex flex-col justify-start items-center gap-4">
-              <MdAutoGraph size={80} className="text-sky-400/80" />
-              <h6 className="text-sky-800 text-center font-bold uppercase">
-                Otimização
-              </h6>
-              <small className="text-center text-pretty">
-                Realizamos o mapeamento completo de todo o fluxo do sistema afim
-                de encontrar pontos de melhoria, desenhando a melhor estratégia
-                para otimização.
-              </small>
-            </div>
-            <div className="w-full flex flex-col justify-start items-center gap-4">
-              <MdCloudSync size={80} className="text-sky-400/80" />
-              <h6 className="text-sky-800 text-center font-bold uppercase">
-                Sustentação
-              </h6>
-              <small className="text-center text-pretty">
-                Com foco em redução de custos e otimização de recursos, optamos
-                por uma arquitetura de fácil implementação, visando uma melhor
-                manutenabilidade e escalabilidade.
-              </small>
-            </div>
-          </div>
-        </div>
-      </section>
-
-      <section className="w-full h-full py-4 bg-gradient-to-t from-white to-slate-200">
-        <div className="w-full mx-auto p-8 sm:max-w-4xl">
-          <div className="w-full flex flex-col sm:flex-row justify-center sm:justify-between items-center gap-4">
-            <figure className="mx-auto w-auto h-auto p-4">
-              <Image
-                src={cloudHosting}
-                alt="infraestrututa dedicado"
-                width={500}
-                height={490}
-                priority
-              />
-            </figure>
-            <div className="w-full flex flex-col justify-center item-center gap-4">
-              <h4 className="text-sky-800 text-center sm:text-left">
-                Infraestrutura SaaS
-              </h4>
-              <h6 className="text-slate-600 text-balance text-center sm:text-left -mt-4">
-                Garantia de alta disponibilidade, segurança escalabilidade
-              </h6>
-              <p className="w-full text-pretty text-center sm:text-left">
-                Todos os serviços de nossas soluções são alocados nas maiores
-                plataformas de computação em nuvem do mundo.
-              </p>
-              <p className="w-full text-pretty text-center sm:text-left">
-                Nossa arquitetura é envolvida por uma camada de isolamento VPC
-                que utiliza protocolos de segurança para a comunicação entre
-                serviços.
-              </p>
-              <p className="w-full text-pretty text-center sm:text-left">
-                Cada serviço de nossas soluções conta com um gerenciador de
-                balanceamento de carga para garantir a disponibilidade em
-                condições de alta demanda.
-              </p>
-              <div className="w-full flex justify-center sm:justify-start">
-                <Link
-                  href={'contato'}
-                  className="w-full max-w-sm p-2 bg-sky-400/80 hover:shadow-md hover:bg-gradient-to-r from-sky-600/80 to-cyan-600/60 hover:opacity-80 rounded-md text-white text-center font-semibold uppercase"
-                >
-                  Saiba mais
-                </Link>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-
-      <section className="w-full h-full py-4 bg-sky-400/80 shadow-md">
+      <section className="w-full h-full py-4 bg-sky-400/80">
         <div className="w-full mx-auto p-8 sm:max-w-4xl">
           <h2 className="leading-tight text-center text-balance text-white lowercase font-light">
             O sistema{' '}
@@ -210,7 +120,8 @@ export default function HomePage() {
                   {' '}
                   sistema de pedidos inteligente
                 </span>
-                , e podemos disponibilizar nossa API para consumo via webservice.
+                , e podemos disponibilizar nossa API para consumo via
+                webservice.
               </p>
               <p className="w-full text-pretty text-center sm:text-right">
                 Nossa estrutura é projetada para suportar milhares de transações
@@ -340,6 +251,96 @@ export default function HomePage() {
                   Quer saber como funciona?
                 </Link>
               </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="w-full h-full py-4">
+        <div className="w-full mx-auto p-8 sm:max-w-4xl">
+          <div className="w-full flex flex-col sm:flex-row justify-center sm:justify-between items-center gap-4">
+            <figure className="mx-auto w-auto h-auto p-4">
+              <Image
+                src={cloudHosting}
+                alt="infraestrututa dedicado"
+                width={500}
+                height={490}
+                priority
+              />
+            </figure>
+            <div className="w-full flex flex-col justify-center item-center gap-4">
+              <h4 className="text-sky-800 text-center sm:text-left">
+                Infraestrutura SaaS
+              </h4>
+              <h6 className="text-slate-600 text-balance text-center sm:text-left -mt-4">
+                Garantia de alta disponibilidade, segurança e escalabilidade
+              </h6>
+              <p className="w-full text-pretty text-center sm:text-left">
+                Todos os serviços de nossas soluções são alocados nas maiores
+                plataformas de computação em nuvem do mundo.
+              </p>
+              <p className="w-full text-pretty text-center sm:text-left">
+                Nossa arquitetura é envolvida por uma camada de isolamento VPC
+                que utiliza protocolos de segurança para a comunicação entre
+                serviços.
+              </p>
+              <p className="w-full text-pretty text-center sm:text-left">
+                Cada serviço de nossas soluções conta com um gerenciador de
+                balanceamento de carga para garantir a disponibilidade em
+                condições de alta demanda.
+              </p>
+              <div className="w-full flex justify-center sm:justify-start">
+                <Link
+                  href={'contato'}
+                  className="w-full max-w-sm p-2 bg-sky-400/80 hover:shadow-md hover:bg-gradient-to-r from-sky-600/80 to-cyan-600/60 hover:opacity-80 rounded-md text-white text-center font-semibold uppercase"
+                >
+                  Saiba mais
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="w-full h-full py-4 bg-white shadow-md">
+        <div className="w-full mx-auto p-8 sm:max-w-4xl">
+          <h2 className="leading-tight text-center text-balance font-light text-sky-800 uppercase">
+            <span className="font-semibold">Cuidamos de tudo</span> pra você
+          </h2>
+          <div className="w-full py-8 flex flex-col sm:flex-row justify-center items-start gap-4">
+            <div className="w-full flex flex-col justify-start items-center gap-4">
+              <MdOutlinePolyline size={80} className="text-sky-400/80" />
+              <h6 className="text-sky-800 text-center font-bold uppercase">
+                Integração
+              </h6>
+              <small className="text-center text-pretty">
+                Integramos sistemas com nossas soluções de gestão de serviços
+                transacionais otimizada com inteligência artificial, e
+                desenvolvemos soluções personalizadas para sistemas já
+                existentes.
+              </small>
+            </div>
+            <div className="w-full flex flex-col justify-start items-center gap-4">
+              <MdAutoGraph size={80} className="text-sky-400/80" />
+              <h6 className="text-sky-800 text-center font-bold uppercase">
+                Otimização
+              </h6>
+              <small className="text-center text-pretty">
+                Realizamos o mapeamento completo de todo o fluxo do sistema afim
+                de encontrar pontos de melhoria, desenhando a melhor estratégia
+                para otimização.
+              </small>
+            </div>
+            <div className="w-full flex flex-col justify-start items-center gap-4">
+              <MdCloudSync size={80} className="text-sky-400/80" />
+              <h6 className="text-sky-800 text-center font-bold uppercase">
+                Sustentação
+              </h6>
+              <small className="text-center text-pretty">
+                Com foco em redução de custos e otimização de recursos, optamos
+                por uma arquitetura de fácil implementação, visando uma melhor
+                manutenabilidade e escalabilidade.
+              </small>
             </div>
           </div>
         </div>
